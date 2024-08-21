fca37240927f193b4e003428cc5588f6dc4bd0b4
FNCRosse <rosita.44.07@gmail.com>
2024-08-21 15:03:09 -0500

    Se ha agregado el lab 2 del 2024_1 sin resolver de P2

---
+++ PROGRAMACION 2/LAB's PASADOS/P2 2024_2 LAB 02/main.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   main.cpp
+ * Author: Rosa Cristina La Cruz Musayon
+ * Codigo: 20213714
+ * Created on 21 de agosto de 2024, 15:02
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
